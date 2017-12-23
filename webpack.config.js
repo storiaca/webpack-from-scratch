c7b61a9a0249b04e8de197f492ba1f7dc5cae58a
storiaca <storiaca@gmail.com>
2017-12-23 12:16:54 +0100

    Add image loaders

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,35 @@ let config = {
                   use: ['css-loader', 'sass-loader'], // use this loaders
                   fallback: 'style-loader' // fallback for any CSS not extracted
                 }) // end extract
-            }
+            },
+            {
+                test: /\.jsx$/, // all files ending with .jsx
+                loader: 'babel-loader', // use the babel-loader for all .jsx files
+                exclude: /node_modules/ // exclude searching for files in the node_modules directory
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                loaders: ['file-loader?context=src/assets/images/&name=images/[path][name].[ext]', {  // images loader
+                    loader: 'image-webpack-loader',
+                    query: {
+                        mozjpeg: {
+                            progressive: true,
+                        },
+                        gifsicle: {
+                            interlaced: false,
+                        },
+                        optipng: {
+                            optimizationLevel: 4,
+                        },
+                        pngquant: {
+                            quality: '75-90',
+                            speed: 3,
+                        },
+                    },
+                }],
+                exclude: /node_modules/,
+                include: __dirname,
+            },
         ] // end rules
     },
     plugins: [
