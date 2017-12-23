cb9ca214f00f922d66b7157310c6432e9e424f8b
storiaca <storiaca@gmail.com>
2017-12-23 13:05:30 +0100

    Add react and images

---
+++ src/app/App.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import cat from '../assets/images/cat.jpg';
+import dog from '../assets/images/dog.png';
+import dogs from '../assets/images/dogs.gif';
+
+
+const hello = () => {
+    return (
+        <div className="container">
+            <div className="image-wrapper">
+                <img src={cat} className="image-wrapper__image" alt=""/>
+            </div>
+
+            <div className="image-wrapper">
+                <img src={dog} className="image-wrapper__image" alt=""/>
+            </div>
+
+            <div className="image-wrapper">
+                <img src={dogs} className="image-wrapper__image" alt=""/>
+            </div>
+        </div>
+    )
+}
+
+export default hello;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 require('./assets/stylesheets/styles.scss');
 
-class Car {
-    manufacturer(car) {
-        document.write(`I have a ${car}`)
-    }
-}
+import React from 'react'; // import the main react dependecy
+import ReactDOM from 'react-dom'; // import reactDom
+import App from './app/App.jsx'; // import the main app component
 
-const bmw = new Car;
-
-bmw.manufacturer('bmw 5');
+ReactDOM.render(<App />, document.getElementById('root')) // render our App component and mount it to our #root element
